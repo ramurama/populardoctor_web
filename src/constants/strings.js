a44b31c6e7ba868b58f7c4b6d609804f2391e4d5
Ramu Ramasamy <rram.ramasamy@gmail.com>
2019-05-21 08:44:24 +0530

    side bar content strings updated

--- src/constants/strings.js
+++ src/constants/strings.js
@@ -1,12 +1,12 @@
-export const USER_MANAGEMENT = 'User Management';
+export const USER_MANAGEMENT = 'Users';
 export const VIEW_USERS = 'View Users';
-export const DOCTOR_MANAGEMENT = 'Doctor Management';
+export const DOCTOR_MANAGEMENT = 'Doctors';
 export const CREATE_DOCTOR = 'Create Doctor';
 export const VIEW_DOCTORS = 'View Doctors';
-export const HOSPITAL_MANAGEMENT = 'Hospital Management';
+export const HOSPITAL_MANAGEMENT = 'Hospitals';
 export const CREATE_HOSPITAL = 'Create Hospital';
 export const VIEW_HOSPITALS = 'View Hospitals';
-export const SCHEDULE_MANAGEMENT = 'Schedule Management';
+export const SCHEDULE_MANAGEMENT = 'Schedules';
 export const CREATE_SCHEDULE = 'Create Schedule';
 export const VIEW_SCHDULES = 'View Schedules';
 export const SETTINGS = 'Settings';
